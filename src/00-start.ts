e0520619493077dbc18bbee2a2b6422781873e75
Chinyi Pam <chinyi.pam@gmail.com>
2023-08-30 11:29:28 +0800

    disable emitting files on type checking errors with terminal

---
+++ src/00-start.ts
@@ -0,0 +1,15 @@
+// Note
+// run 'npx tsc -w' in the terminal
+
+// if you want to
+// disable emitting files if any type checking errors are reported. (暫時性的需求：有錯誤就不要編譯成 js)
+// run 'npx tsc --noEmitOnError -w'
+
+let username = 'Pamcy'
+console.log(username);
+
+let a = 12
+let b = '6'
+let c = 2
+
+console.log(a / b);
--- src/main.ts
+++
@@ -1,6 +0,0 @@
-// Note
-// run 'npx main.ts -w' in the terminal
-
-let username = 'Pamcy'
-console.log(username);
-
