d5f7f49c31fde907ee208d2535ffce6e3ceb05a7
Chinyi Pam <chinyi.pam@gmail.com>
2023-08-30 16:24:19 +0800

    object

--- src/02-array-object.ts
+++ src/02-array-object.ts
@@ -59,3 +59,108 @@ let normalArr = ['racoons', 32, false]
 // Target requires 3 element(s) but source may have fewer.
 
 normalArr = veryStrictArr
+
+
+
+/*
+ *  Object
+ */
+
+let myObj: object
+myObj = []
+
+console.log(typeof myObj)
+// object
+// array is also a obejct in javascript
+
+myObj = mixedData
+myObj = {}
+// 以上這樣寫也都沒問題
+
+
+const exampleObj = {
+    prop1: 'pamcy',
+    prop2: true
+}
+// {
+//     prop1: string;
+//     prop2: boolean;
+// }
+
+// exampleObj.prop1 = 42
+// Type 'number' is not assignable to type 'string'.
+
+exampleObj.prop2 = false
+
+
+// 也可以用
+// type Hero = {}
+interface Hero {
+    name: string,
+    active: boolean,
+    movie: (string | number)[]
+}
+
+// this is type Hero
+let captain: Hero = {
+    name: 'steve rogers',
+    active: false,
+    movie: ['winter soliders', 1980, 'avenger 3']
+}
+
+// let captain: Hero = {
+//     name: 'steve rogers',
+//     movie: ['winter soliders', 1980, 'avenger 3']
+// }
+// Property 'active' is missing in type '{ name: string; movie: (string | number)[]; }' but required in type 'Hero'.
+
+// captain.year = 1988
+// Property 'year' does not exist on type 'Hero'.
+
+
+// 也可以用
+// type Hero = {}
+interface SuperHero {
+    name?: string,  // string | undefined (become union type)
+    active: boolean,
+    movie: (string | number)[]
+}
+
+let ironMan: SuperHero = {
+    name: 'tony stark',
+    active: true,
+    movie: ['I', 'II', 1975]
+}
+
+// 沒有加上 active property 也沒關係
+let thor: SuperHero = {
+    active: false,
+    movie: ['love & thunder', 1980]
+}
+
+
+const greetHero = (hero: SuperHero) => {
+    // return `Hallo ${hero.name.toUpperCase()}`
+    // error: 'hero.name' is possibly 'undefined'.
+
+    // method 1
+    // return `Hallo ${(hero?.name || '').toUpperCase()}`
+
+    // method 2
+    if (hero.name) {
+        return `Hallo ${hero.name.toUpperCase()}`
+    }
+
+    return 'Hallo'
+}
+
+console.log(greetHero(ironMan));
+// Hallo TONY STARK
+
+console.log(greetHero(thor))
+// Hallo
+
+
+// NOTE
+// 之後再討論 type vs. interface 的差別
+// 通常會以 interface 為主
