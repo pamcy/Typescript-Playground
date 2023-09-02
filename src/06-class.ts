2b2be43bf84686ac4fac7e696be66328fe498801
Chinyi Pam <chinyi.pam@gmail.com>
2023-09-02 09:24:27 +0800

    class

---
+++ src/06-class.ts
@@ -0,0 +1,12 @@
+/* =================================================================================
+ Classes (類別)
+
+ - 有點複雜，暫時沒有需求先不深入
+
+ - 教學
+   - https://youtu.be/gieEQFIfgYc?t=7661&si=eWok9ZolGIu9Paci
+   - https://www.typescriptlang.org/docs/handbook/2/classes.html
+   - https://willh.gitbook.io/typescript-tutorial/advanced/class
+     - https://willh.gitbook.io/typescript-tutorial/advanced/class-and-interfaces
+
+================================================================================= */
